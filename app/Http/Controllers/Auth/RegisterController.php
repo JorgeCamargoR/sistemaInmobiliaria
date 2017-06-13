eb75438042ceb7ce992b1e30e9786ed25410f8c2
JotasCR <jotas1_4@hotmail.com>
2017-06-12 19:14:57 -0500

    Create RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/login';
+    protected $redirectTo = '/inventario/inmueble';
 
     /**
      * Create a new controller instance.
